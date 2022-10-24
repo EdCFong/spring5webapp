8378bc52c6e81601f3ad96e4322d3b688afeecf6
Eduardo Castillo Fong <EDUARDCF90@GMAIL.COM>
2022-10-23 23:00:01 -0500

    Add Publisher Entity and Publisher Relationships

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -24,28 +24,43 @@ public class BootStrapData implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
+
+        System.out.println("Started in Bootstrap");
+
+        Publisher blueEditorial = new Publisher("BlueEditorial", "3546 W Palm", "BlueLand", "RE", "77654");
+        publisherRepository.save(blueEditorial);
+        System.out.println("Number of Publishers: " + publisherRepository.count());
+
         Author eric = new Author("Eric", "Evans");
         Book ddd = new Book("Domain Driven Design","123123");
         eric.getBooks().add(ddd);
         ddd.getAuthors().add(eric);
 
+        ddd.setPublisher(blueEditorial);
+        blueEditorial.getBooks().add(ddd);
+
         authorRepository.save(eric);
         bookRepository.save(ddd);
+        publisherRepository.save(blueEditorial);
 
         Author rod = new Author("Rod", "Johnson");
         Book noEJB = new Book("J2EE Development without EJB", "3956875369");
         rod.getBooks().add(noEJB);
         noEJB.getAuthors().add(rod);
 
+        noEJB.setPublisher(blueEditorial);
+        blueEditorial.getBooks().add(noEJB);
+
         authorRepository.save(rod);
         bookRepository.save(noEJB);
-
-        System.out.println("Started in Bootstrap");
-        System.out.println("Number of Books: " + bookRepository.count());
-
-        Publisher blueEditorial = new Publisher("BlueEditorial", "3546 W Palm", "BlueLand", "RE", "77654");
         publisherRepository.save(blueEditorial);
-        System.out.println("Number of Publishers: " + publisherRepository.count());
+
+        System.out.println("Number of Books: " + bookRepository.count());
+        System.out.println("Publisher number of books: " + blueEditorial.getBooks().size());
+
+
+
+
 
     }
 }
